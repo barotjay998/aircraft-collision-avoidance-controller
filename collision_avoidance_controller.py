47cc905dace8e902eac9945a7248b6f26c41dfb6
jay <barotjay998>
2023-12-01 10:24:47 -0600

    init controller

--- collision_avoidance_controller.py
+++ collision_avoidance_controller.py
@@ -1,6 +1,18 @@
 import pandas as pd
 
+
+"""
+Provides methods to read and preprocess the aircraft data.
+"""
 class DatasetReader:
+
+    """
+    Constructor
+    Initializes the DatasetReader object, reads the processed dataset file of type txt
+
+    :param file_path: The path to the processed dataset file.
+    :type file_path: str
+    """
     def __init__(self, file_path):
         self.file_path = file_path
 
@@ -8,7 +20,9 @@ class DatasetReader:
         try:
             with open(self.file_path, 'r') as file:
                 data = [line.strip().split() for line in file]
-                
+
+            # Data headers are based on information broadcased by the aircrafts with 
+            # respect to the ADS-B data format specification.
             columns = ["Frame", "AircraftID", "x", "y", "z", "windx", "windy"]
             df = pd.DataFrame(data, columns=columns, dtype=float)
 
@@ -17,3 +31,28 @@ class DatasetReader:
         except Exception as e:
             print(f"Error reading dataset: {e}")
             return None
+
+
+class CollisionAvoidanceController:
+
+    """
+    Constructor
+    Initializes the CollisionAvoidanceController object, reads the 
+    data sent by the aircrafts in the area (ADS-B data), and stores it in a dataframe.
+
+    :param dataset_df: The dataframe containing the data sent by the aircrafts in the area. 
+    :type data: pandas.DataFrame
+    """
+    def __init__(self, dataset_df):
+        self.dataset_df = dataset_df
+    
+    def run_controller(self):
+        print("Running controller...")
+
+        print(self.dataset_df.head())
+
+        # TODO: Implement the algorithm here.
+        #
+        # TODO: Step 1: COLLISIONDETECTION - Collision Risk-Based Cost Map.
+
+        print("Controller finished running.")
--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-from collision_avoidance_controller import DatasetReader
+from collision_avoidance_controller import DatasetReader, CollisionAvoidanceController
 
 def main():
 
-    # Reading the dataset and storing it in a dataframe.
-
+    # Specify the path to the dataset file
     # dataset_file_path = 'dataset/7days1/processed_data/test/2.txt' # OG dataset.
     dataset_file_path = 'data_read_test_sample.txt'
+
+    # Create an instance of DatasetReader
     dataset_reader = DatasetReader(dataset_file_path)
+
+    # Read the data and get the DataFrame
     dataset_df = dataset_reader.read_dataset()
 
     if dataset_df is not None:
-        print("Dataset read successfully:")
-        print(dataset_df.head())
+        # Create an instance of CollisionAvoidanceController
+        controller = CollisionAvoidanceController(dataset_df)
+
+        # Run the controller
+        controller.run_controller()
 
 if __name__ == "__main__":
     main()
